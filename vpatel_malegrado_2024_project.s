177b33e7257ba10886e7abfcf7d1c4360e36c534
veeralpatel02 <vmpatel@ucsd.edu>
2024-06-01 16:14:02 -0700

    1 and 2

--- vpatel_malegrado_2024_project.s
+++ vpatel_malegrado_2024_project.s
@@ -47,10 +47,17 @@ FindTail:
 	// x0: address of (pointer to) the first symbol of symbol array
 	// output:
 	// x1: address of (pointer to) the first symbol of symbol array
-	ldr x2, [x0, #16]
+
+loop:	
+	ldur x2, [x0, #16]
+	SUBIS XZR, X2, #-1 //compare X2 to -1
+	B.EQ tailfound
 	
-	//compare x2 to -1, if it equals -1, returns pointer
-	
+	ADDI x0, [x0, #16]
+	B loop
+
+tailfound:
+	STUR x1, [x0, #0]
 	br lr
 
 
@@ -65,11 +72,35 @@ FindMidpoint:
 	// x1: address of (pointer to) the last symbol of the symbol array
 	// x2: sum of the frequency of the left sub-array
 	// x3: sum of the frequency of the right sub-array
+loop:    
+	ADD X5,[X0,#16]
+	SUBIS x7, x5, x1     # Compare x5 with x1, setting flags
+    BEQ TailFound        # if (head + 2 == tail) goto TailFound
 	
+	SUBS X6, X2, X3		 # SUBTRACT the two, if greater brach to tail and then tranch to head 
+    B.LE UpdateTail      # if (left sum <= right sum) goto UpdateHead
+	B UpdateHead
+
+UpdateTail:
+    ADDI x0, [x0, #16]
+	LDUR x7, [x0,#8]      # head = head + 2
+    ADD x2, x2, x7        # left sum = left sum + *(head)
+	B ContinueRecursion   # goto ContinueRecursion
+
+UpdateHead:
+	ADDI x1, [x1, #16]
+	LDUR x7, [x1,#8]      # head = head + 2
+    ADD x3, x3, x7        # left sum = left sum + *(head)
+    B ContinueRecursion   # goto ContinueRecursion
+
+ContinueRecursion:
+    BL loop       # Recursive call to FindMidpoint
+
 	// output:
 	// x4: address of (pointer to) the first element of the right-hand side sub-array
-	
-	br lr
+ReturnTail:
+    ADD x4, x1, xzr            # x4 = tail
+    br lr                 # return	
 
 
 ////////////////////////
