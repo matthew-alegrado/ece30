bc645fc08c08f6003a2d5cf78955d336484e5bb4
veeralpatel02 <vmpatel@ucsd.edu>
2024-06-03 16:57:27 -0700

    Updated Head

--- vpatel_malegrado_2024_project.s
+++ vpatel_malegrado_2024_project.s
@@ -51,7 +51,7 @@ FindTail:
 
 FindTail_loop:
 	ldur x2, [x0, #16]
-	subs x4, x2, x3       // Subtract x2 from x3 (symbol) to check if it's -1
+	subs xzr, x3, x2       // Subtract x2 from x3 (symbol) to check if it's -1
 	b.eq tailfound
 	
 	addi x0, [x0, #16]
@@ -74,24 +74,25 @@ FindMidpoint:
 	// x1: address of (pointer to) the last symbol of the symbol array
 	// x2: sum of the frequency of the left sub-array
 	// x3: sum of the frequency of the right sub-array
-FindMidpoint_loop:
-	ADD X5,[X0,#16]
-	SUBS x7, x5, x1     # Compare x5 with x1, setting flags
-    B.EQ TailFound        # if (head + 2 == tail) goto TailFound
-	
-	SUBS X6, X2, X3		 # SUBTRACT the two, if greater brach to tail and then tranch to head 
-    B.LE UpdateTail      # if (left sum <= right sum) goto UpdateHead
-	B UpdateHead
 
-UpdateTail:
-    ADDI x0, [x0, #16]
-	LDUR x7, [x0,#8]      # head = head + 2
+
+FindMidpoint_loop:
+	ADDI x5,x0,#16
+	SUBS x7, x5, x1     # Compare x5 with x1, setting flags
+    B.EQ ReturnTail        # if (head + 2 == tail) goto TailFound
+	
+	SUBS X6, X3, X2		 # SUBTRACT the two, if greater brach to tail and then tranch to head 
+    B.LT UpdateTail      # if (left sum <= right sum) goto UpdateHead
+
+UpdateHead:
+    ADDI x0, x0, #16      # head = head + 2
+	LDUR x7, x0,#8      
     ADD x2, x2, x7        # left sum = left sum + *(head)
 	BL FindMidpoint_loop       # Recursive call to FindMidpoint
 
-UpdateHead:
-	ADDI x1, [x1, #16]
-	LDUR x7, [x1,#8]      # head = head + 2
+UpdateTail:
+	SUBI x1, x1, #16
+	LDUR x7, x1,#8      # head = head + 2
     ADD x3, x3, x7        # left sum = left sum + *(head)
     BL FindMidpoint_loop       # Recursive call to FindMidpoint
 	
